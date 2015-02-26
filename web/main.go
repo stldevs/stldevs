d522f22b169b437f3b23ddf423a3a8c6a9423d56
jake <jake@jakecoffman.com>
2015-02-26 15:25:48 -0600

    back to basics

--- web/main.go
+++ web/main.go
@@ -11,8 +11,25 @@ func main() {
 	fileHandler := http.FileServer(http.Dir("static/"))
 
 	router := httprouter.New()
-	router.GET("/static", fileHandler)
+	router.GET("/static/*filepath", handleFiles(fileHandler))
+	router.GET("/", index)
+	router.NotFound = http.HandlerFunc(notFound)
 
-	log.Println("Serving on", "0.0.0.0:8070")
-	http.ListenAndServe("0.0.0.0:8070", router)
+	log.Println("Serving on", "localhost:80")
+	log.Println(http.ListenAndServe("localhost:80", router))
+}
+
+func handleFiles(fileServer http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		r.URL.Path = p.ByName("filepath")
+		fileServer.ServeHTTP(w, r)
+	}
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/404.html")
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w, r, "static/index.html")
 }
