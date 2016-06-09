4758240763c73540ca72545a0a2bd36a96d9eabe
jake <jake@jakecoffman.com>
2016-06-09 17:45:23 -0500

    remove unnecessary prepared statements; remove users that are no longer in St. Louis

--- aggregator/run.go
+++ aggregator/run.go
@@ -3,7 +3,6 @@ package aggregator
 import (
 	"fmt"
 	"log"
-	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,19 +10,24 @@ import (
 )
 
 func (a *Aggregator) updateUsers(users map[string]struct{}) error {
-	rows, err := a.db.Query(`SELECT login FROM agg_user`)
+	existingUsers := []github.User{}
+	err := a.db.Select(&existingUsers, `SELECT login FROM agg_user`)
 	if err != nil {
 		log.Println("Error querying agg_user")
 		return err
 	}
-	for rows.Next() {
-		user := github.User{}
-		if err = rows.Scan(&user.Login); err != nil {
-			log.Println("Error scanning row")
-			return err
-		}
-		if _, ok := users[*user.Login]; !ok {
-			users[*user.Login] = struct{}{}
+
+	// remove users that no longer come up in search
+	for _, existing := range existingUsers {
+		if _, ok := users[existing]; !ok {
+			_, err = a.db.Exec(`DELETE FROM agg_user WHERE login=?`, existing)
+			if err != nil {
+				log.Println("Error while deleting moved user:", existing)
+			}
+			_, err = a.db.Exec(`DELETE FROM agg_repo WHERE owner=?`, existing)
+			if err != nil {
+				log.Println("Error while deleting moved user's repos", existing)
+			}
 		}
 	}
 
@@ -61,28 +65,17 @@ func (a *Aggregator) updateUsersRepos(user string) error {
 		}
 		checkRespAndWait(resp)
 		for _, repo := range result {
-			// TODO: refactor, IIFE for defer
-			err := func() error {
-				stmt, err := a.db.Prepare(`REPLACE INTO agg_repo VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`)
-				if err != nil {
-					log.Println("Error preparing agg_repo")
-					return err
-				}
-				defer stmt.Close()
-				var pushedAt *time.Time
-				if repo.PushedAt != nil {
-					pushedAt = &repo.PushedAt.Time
-				}
-				_, err = stmt.Exec(repo.Owner.Login, repo.Name, repo.Description, repo.Language, repo.Homepage,
-					repo.ForksCount, repo.NetworkCount, repo.OpenIssuesCount, repo.StargazersCount, repo.SubscribersCount,
-					repo.WatchersCount, repo.Size, *repo.Fork, repo.DefaultBranch, repo.MasterBranch, repo.CreatedAt.Time,
-					pushedAt, repo.UpdatedAt.Time)
-				if err != nil {
-					log.Println("Error executing replace into agg_repo")
-				}
-				return err
-			}()
+			var pushedAt *time.Time
+			if repo.PushedAt != nil {
+				pushedAt = &repo.PushedAt.Time
+			}
+			_, err = a.db.Exec(`REPLACE INTO agg_repo VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+				repo.Owner.Login, repo.Name, repo.Description, repo.Language, repo.Homepage,
+				repo.ForksCount, repo.NetworkCount, repo.OpenIssuesCount, repo.StargazersCount, repo.SubscribersCount,
+				repo.WatchersCount, repo.Size, *repo.Fork, repo.DefaultBranch, repo.MasterBranch, repo.CreatedAt.Time,
+				pushedAt, repo.UpdatedAt.Time)
 			if err != nil {
+				log.Println("Error executing replace into agg_repo")
 				return err
 			}
 		}
@@ -121,36 +114,18 @@ func (a *Aggregator) findStlUsers() (map[string]struct{}, error) {
 func (a *Aggregator) Add(user string) error {
 	u, resp, err := a.client.Users.Get(user)
 	if err != nil {
-		if strings.Contains(err.Error(), "404") {
-			log.Println("Deleting user", user)
-			// remove user from database
-			_, err = a.db.Exec(`DELETE FROM agg_user WHERE login=?`, user)
-			return err
-		}
-		log.Println("Unexpected error occurred in Add")
+		log.Println("Failed getting user details for", user, ":", err)
 		return err
 	}
 	checkRespAndWait(resp)
-	stmt, err := a.db.Prepare(`REPLACE INTO agg_user VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`)
-	if err != nil {
-		log.Println("Failed to prepare replace into for agg_user")
-		return err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(u.Login, u.Email, u.Name, u.Location, u.Hireable, u.Blog, u.Bio, u.Followers, u.Following,
+	_, err = a.db.Exec(`REPLACE INTO agg_user VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+		u.Login, u.Email, u.Name, u.Location, u.Hireable, u.Blog, u.Bio, u.Followers, u.Following,
 		u.PublicRepos, u.PublicGists, u.AvatarURL, u.DiskUsage, u.CreatedAt.Time, u.UpdatedAt.Time)
 	return err
 }
 
 func (a *Aggregator) insertRunLog() error {
-	stmt, err := a.db.Prepare(`INSERT INTO agg_meta VALUES (?)`)
-	if err != nil {
-		log.Println("Error preparing insert into agg_meta")
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(time.Now())
+	_, err := a.db.Exec(`INSERT INTO agg_meta VALUES (?)`, time.Now())
 	if err != nil {
 		log.Println("Error executing insert")
 	}
