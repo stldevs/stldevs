f2c07c38da556fac2aa3acd214d4d65c8cda24b1
Jake Coffman <jake@jakecoffman.com>
2024-07-14 15:13:14 +0000

    fix test

--- web/dev/dev_test.go
+++ web/dev/dev_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/google/go-github/v48/github"
+	"github.com/google/go-github/v52/github"
 	"github.com/jakecoffman/stldevs/db"
 	"github.com/jakecoffman/stldevs/sessions"
 	"net/http/httptest"
